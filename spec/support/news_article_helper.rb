9f7b027cdc2016a63ec0589d56a0af820e06d12c
Shalom Ayidu <shalom.ayidu@andela.com>
2018-05-29 00:15:46 +0100

    WIP Setup Support File based on News Response

---
+++ spec/support/news_article_helper.rb
@@ -0,0 +1,15 @@
+require 'ostruct'
+
+module NewsArticleHelper
+
+  def stub_article_headline(headline: default_headline)
+    allow_any_instance_of(News).to receive(:unknown).and_return([headline])
+  end
+
+  def default_headline
+    OpenStruct.new({
+      name: 'JaneNews', url: 'http://localhost:3000/'
+    })
+  end
+
+end

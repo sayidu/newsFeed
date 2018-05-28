b33f4edab0d0b0b07c27a6abbc360192187e4b01
Shalom Ayidu <shalom.ayidu@andela.com>
2018-05-29 00:24:59 +0100

    WIP Update Controller and Feature Specs

--- app/controllers/news_feed_controller.rb
+++ app/controllers/news_feed_controller.rb
@@ -1,4 +1,3 @@
 class NewsFeedController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- spec/controllers/news_feed_controller_spec.rb
+++ spec/controllers/news_feed_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe NewsFeedController, type: :controller do
+  describe 'GET #index' do
+    it 'return index template' do
+      stub_article_headline
 
-  describe "GET #index" do
-    it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+
+      expect(response).to render_template(:index)
     end
   end
-
 end
